1b1f23ca3ee2d2d7da56a2326049fea163449ced
Jamie Burton <jamiejburton@hotmail.co.uk>
2014-05-06 08:55:10 +0100

    Update
    update

---
+++ src/com/example/menyou/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.menyou;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		// Plays music at the startup
+
+		//Buttons with intents
+		//First Button:
+		//		1.Gallery
+		//		2.CallLog
+		//		3.ContactBook
+		Button myGalleryButton = (Button) findViewById(R.id.myGalleryButton);
+		Button myCallLogButton = (Button) findViewById(R.id.myCallLogButton);
+		Button myContactBookButton = (Button) findViewById(R.id.myContactBookButton);
+
+		//OnClick Listeners for Buttons:
+		//		1.Gallery
+		//		2.CallLog
+		//		3.ContactBook
+		myGalleryButton.setOnClickListener(new Button.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent myIntent = new Intent();
+
+				myIntent.setAction(Intent.ACTION_VIEW);
+				myIntent.setData(android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI); // Link to Image Gallery
+				startActivity(myIntent);
+
+			}
+
+		});
+		myCallLogButton.setOnClickListener(new Button.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent myIntent = new Intent(); 
+
+				myIntent.setAction(Intent.ACTION_CALL_BUTTON); // Link to Call Log
+				startActivity(myIntent); //Starts the intent activity
+
+			}
+
+		});
+		myContactBookButton.setOnClickListener(new Button.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent myIntent = new Intent();
+
+				myIntent.setAction(Intent.ACTION_VIEW);
+				myIntent.setData(android.provider.Contacts.People.CONTENT_URI); // Link to Contact Book
+				startActivity(myIntent);
+
+			}
+
+		});
+	}
+
+}
